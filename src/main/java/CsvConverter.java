e87243e48cd5e55cbf0f514992cb78ca3becc7ff
psgrove <ps.grove@gmail.com>
2019-12-23 19:28:16 +0000

    Initial commit

---
+++ src/main/java/CsvConverter.java
@@ -0,0 +1,46 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+
+import java.io.*;
+import java.util.*;
+
+public class CsvConverter {
+
+    private final File file;
+    String fileName;
+
+    public static void main(String[] args) throws IOException {
+       new CsvConverter(null, "Book1.csv");
+    }
+
+    private CsvConverter(File file ,  String filename) throws IOException {
+        this.file = file;
+        this.fileName = filename;
+        Reader in =null;
+        if(null!=filename){
+            Class clazz = CsvConverter.class;
+            InputStream inputStream = clazz.getResourceAsStream(filename);
+            in = new InputStreamReader(inputStream);
+        }
+        else if(null!=file){
+            in = new FileReader(file);
+        }
+
+        if (null!=in){
+            convertToJson(in);
+        }
+        else{
+            throw new RuntimeException("All poo");
+        }
+    }
+
+    private void convertToJson(Reader in) throws IOException {
+        CSVParser csvParser = CSVFormat.EXCEL.withFirstRecordAsHeader().parse(in);
+        List<Map<String, String>> data = new ArrayList<>();
+        csvParser.getRecords().forEach(record -> data.add(record.toMap()));
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonResult = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(data);
+        System.out.println(jsonResult);
+    }
+}
