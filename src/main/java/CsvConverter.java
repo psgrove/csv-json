96f0ac35fef6cb2c202b057ced189c1bfa1dff6a
paul.grove <Paul.Grove@greensill.com>
2019-12-24 12:21:04 +0000

    Added handling BOM

--- src/main/java/CsvConverter.java
+++ src/main/java/CsvConverter.java
@@ -1,42 +1,41 @@
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
+import org.apache.commons.io.input.BOMInputStream;
 
 import java.io.*;
+import java.time.Instant;
 import java.util.*;
 
 public class CsvConverter {
 
     private final File file;
-    String fileName;
-    String outFileName = "output.json";
+    private String fileName;
+    private String outFileName = "output-"+ Instant.now().getEpochSecond()+".json";
+    private Reader in =null;
 
     public static void main(String[] args) throws IOException {
-       new CsvConverter(null, "Book1.csv");
+        CsvConverter converter = new CsvConverter(null, "Book1.csv");
+        converter.convertToJson();
     }
 
     private CsvConverter(File file ,  String filename) throws IOException {
         this.file = file;
         this.fileName = filename;
-        Reader in =null;
-        if(null!=filename){
+    }
+
+    private void convertToJson() throws IOException {
+        if(null!=fileName){
             Class clazz = CsvConverter.class;
-            InputStream inputStream = clazz.getResourceAsStream(filename);
+            InputStream inputStream = new BOMInputStream( clazz.getResourceAsStream(fileName));
             in = new InputStreamReader(inputStream);
         }
         else if(null!=file){
             in = new FileReader(file);
         }
-
-        if (null!=in){
-            convertToJson(in);
-        }
-        else{
+        if(null==in){
             throw new RuntimeException("All poo");
         }
-    }
-
-    private void convertToJson(Reader in) throws IOException {
         CSVParser csvParser = CSVFormat.EXCEL.withFirstRecordAsHeader().parse(in);
         List<Map<String, String>> data = new ArrayList<>();
         csvParser.getRecords().forEach(record -> data.add(record.toMap()));
